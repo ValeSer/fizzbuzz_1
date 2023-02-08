76a624c0bd761b672961b1707889c36469a31965
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-08 16:36:46 +0000

    Implement code for non int

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,13 @@
  def fizzbuzz(number)
-    if number % 3 == 0 && number % 5 == 0
-      return 'fizzbuzz'
+    if !number.is_a? Integer 
+        return "not working"
+    elsif number % 3 == 0 && number % 5 == 0
+       return 'fizzbuzz'
     elsif number % 3 == 0 
-      return 'fizz'
+       return 'fizz'
     elsif number % 5 == 0
       return 'buzz'
+    else
+        number.to_s
     end
  end
