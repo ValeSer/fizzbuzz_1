f6a3738a3aa52cbd9811a262cfe444161ef0944e
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-08 16:36:30 +0000

    test for number and for non int

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -24,4 +24,16 @@ describe 'fizzbuzz' do
     it 'returns "buzz" when passed multiple of 5' do
         expect(fizzbuzz(50)).to eq "buzz"
     end
+    it 'returns "nil" when passed non integer' do
+        expect(fizzbuzz("a")).to eq "not working"
+    end
+    it 'returns "number" when passed a number' do
+        expect(fizzbuzz(2)).to eq '2'
+    end
+    it 'returns "number" when passed a number' do
+        expect(fizzbuzz(4)).to eq '4'
+    end
+    it 'returns "number" when passed a number' do
+        expect(fizzbuzz(7)).to eq '7'
+    end
 end
